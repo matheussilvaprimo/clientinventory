49aeeef7aaa9332eacddccd54fb9c90e2833d99d
matheussilvaprimo <matheussilvaprimo@hotmail.com>
2018-04-01 17:36:49 -0300

    few upgrades

--- src/infra/Infra.Exceptions/BaseException.cs
+++ src/infra/Infra.Exceptions/BaseException.cs
@@ -4,7 +4,15 @@ namespace Infra.Exceptions
 {
     public abstract class BaseException : Exception
     {
+        /// <summary>
+        /// User friendly error code
+        /// </summary>
+        public int CustomCode { get; set; }
+        /// <summary>
+        /// User friendly message 
+        /// </summary>
+        public string CustomMessage { get; set; }
         public BaseException(string msg) : base(msg) { }
-        public BaseException(string msg, Exception inner) : base(msg, inner) { }
+        public BaseException(string msg, Exception inner) : base(msg, inner) { }    
     }
 }
